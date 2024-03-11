adb46db3fc4ee107439e7a594eb32c7b1f1a22c4
Kyle-Anthony Hay <haykyle917@gmail.com>
2024-03-10 22:13:31 -0400

    Work Card Added

--- PortfolioSite/src/pages/Desktop.js
+++ PortfolioSite/src/pages/Desktop.js
@@ -1,12 +1,15 @@
 import AboutContainer from "../components/AboutContainer";
+import ColorfulConnectContainer from "../components/ColorfulConnectContainer";
 import ContentSection from "../components/ContentSection";
 import "./Desktop.css";
+import WorkSection from "./WorkSection";
 
 const Desktop = () => {
   return (
     <div className="desktop-1">
       <AboutContainer />
       <ContentSection />
+      <WorkSection />
     </div>
   );
 };
---
+++ PortfolioSite/src/pages/WorkSection.js
@@ -0,0 +1,13 @@
+import ColorfulConnectContainer from "../components/ColorfulConnectContainer";
+import "./WorkSection.css";
+
+const WorkSection = () => {
+  return (
+    <div className="worksection">
+      <b className="work">Work</b>
+      <ColorfulConnectContainer />
+    </div>
+  );
+};
+
+export default WorkSection;
