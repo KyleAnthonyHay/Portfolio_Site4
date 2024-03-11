f7073039577c0740c0f2df006311de47e0829038
Kyle-Anthony Hay <haykyle917@gmail.com>
2024-03-10 20:16:47 -0400

    links added

--- PortfolioSite/src/components/AboutContainer.js
+++ PortfolioSite/src/components/AboutContainer.js
@@ -5,18 +5,23 @@ const AboutContainer = () => {
     <div className="navbar">
       <div className="links">
         <img className="faceemoji-icon" alt="" src="/faceemoji@2x.png" />
-        <div className="about">About</div>
-        <div className="about">Work</div>
-        <div className="about">Blog</div>
-        <div className="about">Contact</div>
+        <div className="navbar-links">About</div>
+        <div className="navbar-links">Work</div>
+        <div className="navbar-links">Contact</div>
       </div>
       <div className="socialicons">
-        <img className="linkedin-icon" alt="" src="/linkedin.svg" />
-        <img className="x-icon" alt="" src="/x.svg" />
+        <a href="https://www.linkedin.com/in/kyle-anthonyhay/" target="_blank">
+          <img className="linkedin-icon" alt="LinkedIn" src="/linkedin.svg" />
+        </a>
+        <a href="https://twitter.com/KyleAnthonyHay" target="_blank">
+          <img className="x-icon" alt="" src="/x.svg" />
+        </a>
+        <a href="https://github.com/KyleAnthonyHay" target="_blank">
         <img className="github-icon" alt="" src="/github.svg" />
+        </a>
         <div className="line" />
-        <img className="linkedin-icon" alt="" src="/sun.svg" />
-        <div className="whitespace" />
+        {/* <img className="linkedin-icon" alt="" src="/sun.svg" />
+        <div className="whitespace" /> */}
       </div>
     </div>
   );
