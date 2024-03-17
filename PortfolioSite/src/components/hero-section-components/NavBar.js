df51fa4247c661d65801a3000db4bb6ba369abe5
Kyle-Anthony Hay <haykyle917@gmail.com>
2024-03-16 21:18:45 -0400

    Name and Icon added, icons switched, reportvitals file removed

--- PortfolioSite/src/components/hero-section-components/NavBar.js
+++ PortfolioSite/src/components/hero-section-components/NavBar.js
@@ -23,15 +23,9 @@ const AboutContainer = () => {
 
       </div>
       <div className="socialicons">
-        <a href="https://www.linkedin.com/in/kyle-anthonyhay/" target="_blank">
-          <img className="social-icon" alt="LinkedIn" src="/linkedin.svg" />
-        </a>
-        <a href="https://twitter.com/KyleAnthonyHay" target="_blank">
-          <img className="social-icon" alt="" src="/x.svg" />
-        </a>
-        <a href="https://github.com/KyleAnthonyHay" target="_blank">
-        <img className="social-icon" alt="" src="/github.svg" />
-        </a>
+        <a href="https://www.linkedin.com/in/kyle-anthonyhay/" target="_blank"><img className="social-icon" alt="LinkedIn" src="/linkedin.svg" /></a>
+        <a href="https://github.com/KyleAnthonyHay" target="_blank"><img className="social-icon" alt="" src="/github.svg" /></a>
+        <a href="https://twitter.com/KyleAnthonyHay" target="_blank"><img className="social-icon" alt="" src="/x.svg" /></a>
         {/* <div className="line" /> */}
         {/* <img className="linkedin-icon" alt="" src="/sun.svg" />
         <div className="whitespace" /> */}
